8fa062192afd10279f0ea783136cae2288ab4f85
Ulanbek Bolotbekov <drreal@mail.ru>
2021-12-29 20:58:52 +0300

    Добавьте файлы проекта.

---
+++ CA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcatArray
+{
+    public static class CA
+    {
+        public static void ConcatArray(string[] nums, string target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target && i != j)
+                    {
+                        Console.WriteLine($"{(i, j)}: \"{nums[i]}\" + \"{nums[j]}\"");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConcatArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nums = new string[] { "777", "7", "77", "77" };
+            string target = "7777";
+            CA.ConcatArray(nums, target);
+            string[] nums1 = new string[] { "123", "4", "12", "34" };
+            string target1 = "1234";
+            CA.ConcatArray(nums1, target1);
+
+        }
+    }
+}
